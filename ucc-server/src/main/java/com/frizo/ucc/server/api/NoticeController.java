84795e5116cf694db8fe2ab8d112f44bad6222e9
Johnny <Jarvan1110@gmail.com>
2020-12-05 12:05:23 +0800

    fix bug: notice restfulAPI not work

--- ucc-server/src/main/java/com/frizo/ucc/server/api/NoticeController.java
+++ ucc-server/src/main/java/com/frizo/ucc/server/api/NoticeController.java
@@ -12,9 +12,11 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController("/notice")
+@RestController
+@RequestMapping("/notice")
 public class NoticeController {
 
     @Autowired
